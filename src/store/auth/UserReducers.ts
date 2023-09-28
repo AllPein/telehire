f2a215c64389a74cf6020a9294450064bc05c239
a.o.panin <a.o.panin@tinkoff.ru>
2023-09-28 22:13:47 +0300

    company creation

--- src/store/auth/UserReducers.ts
+++ src/store/auth/UserReducers.ts
@@ -1,39 +1,31 @@
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
 
-import { Company } from '@/components/models/Company';
-import { TelegramUser } from '@/components/models/User';
+import { User } from '@/components/models/User';
 import { token } from '@/utils/token';
 import { UserAction, UserStore } from './UserActions';
 
 export const userStoreInitialState: UserStore = {
-  companyList: null,
-  userInfo: (window as any).Telegram.WebApp.initDataUnsafe.user,
-  //   userInfo: {
-  //     id: '279058397',
-  //     first_name: 'Aleksandr',
-  //     last_name: 'Panin',
-  //     username: 'allpein',
-  //     photo_url: ''
-  //   },
+  //   userInfo: null,
+  userInfo: {
+    id: '279058397',
+    first_name: 'Aleksandr',
+    last_name: 'Panin',
+    username: 'allpein',
+    photo_url: '',
+    companyList: null,
+    loggedInAs: null,
+  },
   token: token,
 };
 
 export const UserReducers = reducerWithInitialState<UserStore>(
   userStoreInitialState,
 )
-  .case(
-    UserAction.setCompanyList,
-    (state: UserStore, companyList: Company[]) => {
-      return {
-        ...state,
-        companyList,
-      };
-    },
-  )
-  .case(UserAction.setUser, (state: UserStore, user: TelegramUser) => {
+  //@ts-ignore
+  .case(UserAction.setUser, (state: UserStore, user: Partial<User>) => {
     return {
       ...state,
-      userInfo: user,
+      userInfo: { ...state.userInfo, ...user },
     };
   })
   .case(UserAction.setToken, (state: UserStore, token: string) => {
