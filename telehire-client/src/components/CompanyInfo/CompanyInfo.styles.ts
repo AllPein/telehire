676fa2db09ad19bb9aefccc0e06ca79e3e22f6a7
a.o.panin <a.o.panin@tinkoff.ru>
2023-10-10 22:34:27 +0300

    try to fix width

--- telehire-client/src/components/CompanyInfo/CompanyInfo.styles.ts
+++ telehire-client/src/components/CompanyInfo/CompanyInfo.styles.ts
@@ -4,7 +4,6 @@ export const JobInfoWrapper = styled.div`
   background-color: var(--tg-theme-secondary-bg-color);
   border-radius: 8px;
   padding: 16px;
-  width: 100%;
   flex-direction: column;
   display: flex;
   justify-content: center;
--- telehire-client/src/components/Layout/Layout.styles.ts
+++ telehire-client/src/components/Layout/Layout.styles.ts
@@ -7,7 +7,6 @@ type WrapperProps = {
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
   padding: 52px 16px;
   align-items: center;
 `;
