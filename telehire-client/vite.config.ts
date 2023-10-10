288cf28cc13ba3d6622fe85465db9df50efcb7d6
Andrei Nebogatikov <andreynebogatikof@gmail.com>
2023-10-10 20:28:51 +0300

    docker fixes

--- telehire-client/vite.config.ts
+++ telehire-client/vite.config.ts
@@ -6,6 +6,10 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
